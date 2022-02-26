72c039c44a6a169e29cff00d9313208b69989954
Devin Trejo <dtrejod@users.noreply.github.com>
2022-02-26 10:46:06 -0500

    Setup oh-my-bash and remove sshrc (#12)

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -sf "$CUR_DIR/pkgs/bash/bashrc" ~/.bashrc
 ln -sf "$CUR_DIR/pkgs/vim/vimrc" ~/.vimrc
 ln -sf "$CUR_DIR/pkgs/tmux/tmux.conf" ~/.tmux.conf
 ln -sf "$CUR_DIR/pkgs/bash/inputrc" ~/.inputrc
+ln -sf "$CUR_DIR/submodules/oh-my-bash" ~/.oh-my-bash
 if [[ ! -d ~/.config/nvim/init.vim ]]; then
     mkdir -p ~/.config/nvim
 fi
@@ -33,18 +34,6 @@ if [[ -e ~/.local/bin/ ]]; then
    for s in $(ls $CUR_DIR/bin/*.*); do
       ln -sf "$s" ~/.local/bin/
    done
-
-   # SSHRC setup
-   if ! which sshrc 1>/dev/null 2>&1; then
-      if [[ -e "$CUR_DIR/submodules/sshrc/sshrc" ]]; then
-         ln -sf "$CUR_DIR/submodules/sshrc/sshrc" ~/.local/bin/
-         ln -sf ~/.bashrc ~/.sshrc
-         mkdir ~/.sshrc.d/
-         ln -sf ~/.vimrc ~/.sshrc.d/
-      else
-         echo "WARN: sshrc not initialized. Run 'git submodule init && git submodule update'"
-      fi
-   fi
 else
    echo "WARN: '~/.local/bin' does not exist."
 fi
